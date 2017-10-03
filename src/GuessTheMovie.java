dfe42a1f5a9c96f957407f5a3514548e4ef3697f
MEURANT <alexandremeurant@hotmail.fr>
2017-10-03 18:50:44 +0200

    Create a movies array and add movie to it from the file text

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -9,6 +9,17 @@ public class GuessTheMovie {
         File file = new File("movies.txt");
         Scanner scanner = new Scanner(file);
 
+        // Create a movies array and add movie to it from the file text:
+        int movieNumber = 0;
+        String[] movies = new String[26];
+
+        while(scanner.hasNextLine()){
+            String movie = scanner.nextLine();
+            movies[movieNumber] = movie;
+            movieNumber++;
+        }
+
+
     }
 
 }
