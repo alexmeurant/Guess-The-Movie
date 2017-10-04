595c45059d7c61dacc8a18f90094cdffe94de6aa
MEURANT <alexandremeurant@hotmail.fr>
2017-10-04 13:29:49 +0200

    Changed Array to Arraylist

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -9,19 +10,21 @@ public class GuessTheMovie {
         File file = new File("movies.txt");
         Scanner scanner = new Scanner(file);
 
-        // Create a movies array and add movie to it from the file text:
-        int movieNumber = 0;
-        String[] movies = new String[26];
+        // Create an empty movies' array and add movie to it from the file text:
+        ArrayList movies = new ArrayList();
+
 
         while(scanner.hasNextLine()){
             String movie = scanner.nextLine();
-            movies[movieNumber] = movie;
-            movieNumber++;
+            movies.add(movie);
         }
 
         // Pick a random movie to guess:
-        int randomNumber = (int) (Math.random()*26) + 1;
-        String randomMovie = movies[randomNumber];
+        int size = movies.size();
+        int randomNumber = (int) (Math.random()*size);
+        System.out.println(randomNumber);
+        String randomMovie = (String) movies.get(randomNumber);
+        System.out.println(randomMovie);
 
     }
 }
