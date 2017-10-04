3cc83b551edc9e1be4e62f14bb030bcdece94709
MEURANT <alexandremeurant@hotmail.fr>
2017-10-04 15:20:47 +0200

    Added a "hiddenMovie" String to keep "randomMovie" String

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -9,6 +9,7 @@ public class GuessTheMovie {
         // Added movie.txt file to scan it:
         File file = new File("movies.txt");
         Scanner scanner = new Scanner(file);
+        Scanner userGuess = new Scanner(System.in);
 
         // Create an empty movies' array and add movie to it from the file text:
         ArrayList movies = new ArrayList();
@@ -23,13 +24,22 @@ public class GuessTheMovie {
         int size = movies.size();
         int randomNumber = (int) (Math.random()*size);
         String randomMovie = (String) movies.get(randomNumber);
+        String hiddenMovie = randomMovie;
 
         // Convert randomMovie's letters to '_':
         for (int i = 0; i < randomMovie.length(); i++){
             char movieChar = randomMovie.charAt(i);
             if (movieChar != ' ' && movieChar != ':' && movieChar != '-'){
-                randomMovie = randomMovie.replace(movieChar, '_');
+                hiddenMovie = hiddenMovie.replace(movieChar, '_');
             }
         }
+
+        // Invite the player to guess the random hidden movie :
+        System.out.println("You are guessing: " + hiddenMovie);
+
+        int guess = 0;
+        System.out.println("You have guessed (" + guess + ") wrong letters:");
+        System.out.println("Guess a letter: ");
+
     }
 }
